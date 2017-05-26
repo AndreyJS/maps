182a48dd84517ae29646e0fa346f31c83a8cc12c
Myagchenkov Andrey <myagchenkov.andrey@gmail.com>
2017-05-26 09:12:01 +0300

    add delete & format view

--- src/app/map2/map2.component.ts
+++ src/app/map2/map2.component.ts
@@ -7,23 +7,25 @@ import { DragulaService } from 'ng2-dragula';
     styleUrls: ['./map2.component.scss']
 })
 export class Map2Component implements OnInit {
-    public newAddress;
+    public newAddress: string;
     public addressArr = [];
 
     constructor(private DragulaService: DragulaService) {
         DragulaService.dropModel.subscribe((value) => {
-            console.log(value);
-            console.log(value.slice(1));
             this.onDropModel(value.slice(1));
         });
         DragulaService.removeModel.subscribe((value) => {
-        this.onRemoveModel(value.slice(1));
+            this.onRemoveModel(value.slice(1));
         });
     }
     
     public addAddress() {
         this.addressArr.push(this.newAddress);
         this.newAddress = undefined;
+    }
+
+    public delAddress(i: number) {
+        this.addressArr.splice(i, 1);
     }
 
     private onDropModel(args) {
