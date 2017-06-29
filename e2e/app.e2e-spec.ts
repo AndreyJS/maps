59ce9a9be339c89d1f4961bb231b1c69712336c6
Myagchenkov Andrey <myagchenkov.andrey@gmail.com>
2017-06-29 09:56:29 +0300

    write tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,54 +1,42 @@
-import { browser, element, by } from 'protractor';
-import { MapsPage } from './app.po';
+import { browser, by, element, protractor, Key, WebDriver } from 'protractor';
 
-let page: MapsPage;
-
-// describe('maps App', () => {
-// 	beforeEach(() => {
-// 		page = new MapsPage();
-// 	});
-
-// 	it('should display message saying app works', () => {
-// 		page.navigateTo();
-// 		expect(page.getParagraphText()).toEqual('app works!');
-// 	});
-// });
-
-// describe('test', () => {
-// 	beforeEach(() => {
-// 		page = new MapsPage();
-// 	});
-// 	it('should have a title', () => {
-// 		page.navigateTo();
-// 		expect(browser.getTitle()).toEqual('Maps');
-// 	});
-// });
-
-describe('Protractor Demo App', () => {
-	let first = element(by.model('first'));
-	let second = element(by.model('second'));
-	let goBtn = element(by.id('gobutton'));
-	let latestResult = element(by.binding('latest'));
-	let history = element.all(by.repeater('result in memory'));
-
-	function add(a, b) {
-		first.sendKeys(a);
-		second.sendKeys(b);
-		goBtn.click();
-	}
+describe('Maps testing', () => {
+    let cities = ['moscow', 'protvino', 'обнинск'];
+    let input = element(by.css('input'));
+    let addressArr = element(by.repeater('let item of addressArr; let i = index'));
 
 	beforeEach(() => {
-		browser.get('http://juliemr.github.io/protractor-demo/');
+		browser.get('/');
+        for (let i = 0; i < cities.length; i++) {
+            input.sendKeys(cities[i] + '\n');
+        }
 	});
 
-	it('should have a history', () => {
-		add(1, 2);
-		add(3, 4);
-
-		expect(history.count()).toEqual(2);
-
-		add(5, 6);
-
-		expect(history.count()).toEqual(3);
+	it('should to add three address', () => {
+        expect(addressArr.count()).toEqual('3');
 	});
+
+    it('should to delete address', () => {
+        addressArr.row[1].element(by.css('.material-icons')).click();
+        expect(addressArr.count()).toEqual('2');
+    });
+
+    it('should to move element', () => {
+        let allAddress = element.all(by.binding('item.address'));
+        expect(allAddress.get(0).getText()).toEqual('moscow');
+
+        browser.actions()
+            .dragAndDrop(addressArr.row[1], addressArr.row[2])
+            .mouseUp()
+            .perform();
+
+        expect(allAddress.get(0).getText()).toEqual('protvino');
+    });
+
+    it('should have a infoWindow', () => {
+        let marker = element(by.css('img[src$="spotlight-poi.png"]'));
+        marker.click();
+        let info = element(by.xpath('/div[text() = "moscow"'));
+        expect(info.isPresent()).toBe(true);
+    });
 });
